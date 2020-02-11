cc0cb02638e59edd9489eacdae64c06aac54012a
Remco Peggeman <r.peggeman@youngcapital.nl>
2020-02-11 16:22:21 +0100

    Add a presentation as example, fix the copy because not all links will be to YouTube

--- loadPresentations.js
+++ loadPresentations.js
@@ -1,33 +1,60 @@
 const presentations = [
-  // { 
-  //   title: 'Placeholder',
-  //   description: 'Lorem ipsum solor bla bla bla',
-  //   presentation: '',
-  //   video: ''
-  // },
+  {
+    title: 'The Road to Understanding Monads: Part 2',
+    description: 'In this talk Remco describes what categories are, how they relate to types, and what functors and monads are.',
+    slides: 'https://docs.google.com/presentation/d/153ECriBVdBWWrrgKFiBR09RY6rNb6rsGYgccpxWXiJE/edit?usp=sharing',
+    video: 'https://drive.google.com/file/d/1P9oYU_JlMrhVf4AFILwnUvDv12gQEvL_/view?usp=sharing'
+  },
 ];
+
+function getTemplate(templateName) {
+  const template = document.getElementById(templateName);
+  return template.content.cloneNode(true);
+}
 
 function fillTemplate(node, key, value, attribute = 'innerHTML') {
   node.querySelector(`[data-template="${key}"]`)[attribute] = value;
   return node;
 }
 
+function renderLink(text, url) {
+  const template = getTemplate('link');
+
+  fillTemplate(template, "text", text);
+  fillTemplate(template, "link", url, 'href');
+
+  return template;
+}
+
 function renderPresentation(presentation) {
-  const template = document.getElementById('presentation');
-  const templateContent = template.content.cloneNode(true);;
+  const template = getTemplate('presentation');
+  const card = template.querySelector(`[data-template="card"]`);
 
-  fillTemplate(templateContent, "name", presentation.title);
-  fillTemplate(templateContent, "description", presentation.description);
-  fillTemplate(templateContent, "video-link", presentation.video, 'href');
-  fillTemplate(templateContent, "presentation-link", presentation.presentation, 'href');
+  fillTemplate(template, "name", presentation.title);
+  fillTemplate(template, "description", presentation.description);
 
+  if (presentation.video) {
+    const link = renderLink('Link to video', presentation.video);
+    card.appendChild(link);
+  }
+
+  if (presentation.slides) {
+    const link = renderLink('Link to slides', presentation.slides);
+    card.appendChild(link);
+  }
+
+  return template;
+}
+
+function appendPresentation(renderedTemplate) {
   const presentationsContainer = document.getElementById('presentations-container');
-
-  presentationsContainer.appendChild(templateContent);
+  presentationsContainer.appendChild(renderedTemplate);
 }
 
 function loadPresentations() {
-  presentations.forEach(presentation => renderPresentation(presentation));
+  presentations
+    .map(presentation => renderPresentation(presentation))
+    .map(renderedTemplate => appendPresentation(renderedTemplate));
 }
 
 window.addEventListener('load', () => loadPresentations());
